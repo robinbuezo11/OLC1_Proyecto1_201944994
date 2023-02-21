900ad3f1ca7618b33242abf681b4364ec7d42e4b
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2023-02-21 00:27:33 -0600

    [add] proyecto java, jflex y cup

---
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author Robin
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
